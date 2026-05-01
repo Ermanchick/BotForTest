2374d2513187cf94b936686688fa4a9d5f6707cc
Ermanchick <ib645332@gmail.com>
2026-05-02 00:36:31 +0500

    Добавьте файлы проекта.

---
+++ BotForTest/Program.cs
@@ -0,0 +1,60 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+class Program
+{
+    private static string token = "8771012832:AAE2P_M4lk-kLwIQUGmGrpT3REu0GDtCWBc";
+
+    static async Task Main()
+    {
+        var bot = new TelegramBotClient(token);
+
+        using var cts = new CancellationTokenSource();
+
+        var receiverOptions = new ReceiverOptions
+        {
+            AllowedUpdates = Array.Empty<UpdateType>()
+        };
+
+        bot.StartReceiving(
+            updateHandler: HandleUpdateAsync,
+            errorHandler: HandleErrorAsync,
+            receiverOptions: receiverOptions,
+            cancellationToken: cts.Token
+        );
+
+        var me = await bot.GetMe();
+        Console.WriteLine($"Bot started: @{me.Username}");
+
+        Console.ReadLine();
+        cts.Cancel();
+    }
+
+    private static async Task HandleUpdateAsync(
+        ITelegramBotClient bot,
+        Update update,
+        CancellationToken ct)
+    {
+        if (update.Message is not { } message) return;
+        if (message.Text is not { } text) return;
+
+        Console.WriteLine($"User: {text}");
+
+        await bot.SendMessage(
+            chatId: message.Chat.Id,
+            text: $"Ты написал: {text}",
+            cancellationToken: ct
+        );
+    }
+
+    private static Task HandleErrorAsync(
+        ITelegramBotClient bot,
+        Exception exception,
+        CancellationToken ct)
+    {
+        Console.WriteLine($"Error: {exception.Message}");
+        return Task.CompletedTask;
+    }
+}
